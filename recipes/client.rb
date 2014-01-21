6ce15f4f28f782c0fe35cb46a46557d8721c6a03
Fraser Scott <fraser.scott@gmail.com>
2014-01-21 10:24:15 +0000

    yum v3

--- recipes/client.rb
+++ recipes/client.rb
@@ -2,7 +2,7 @@ case node['platform_family']
 when 'debian'
   include_recipe 'apt'
 when 'rhel'
-  include_recipe 'yum::epel'
+  include_recipe 'yum-epel'
 end
 
 directory node['sensu_spec']['conf_dir'] do
