213859836a2425e53fb3418617792a6601488033
Fraser Scott <fraser.scott@gmail.com>
2014-02-21 16:12:47 +0000

    fixing grep in check_cmd

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'sensu_spec'
 maintainer       'Fraser Scott'
-maintainer_email 'fraser.scott@burberry.com'
+maintainer_email 'fraser.scott@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures sensu_spec'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.2'
 
 depends 'apt', '~> 2.3'
 depends 'yum', '~> 3.0'
