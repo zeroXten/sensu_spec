2664f8ac0f23aa3f6f2bcfea90960e0af5b10694
Fraser Scott <fraser.scott@gmail.com>
2014-09-09 16:12:07 +0100

    deps

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'fraser.scott@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures sensu_spec'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.10.0'
+version          '0.10.1'
 
-depends 'apt', '~> 2.3'
-depends 'yum', '~> 3.0'
-depends 'yum-epel', '~> 0.2'
+depends 'apt', '>= 2.6'
+depends 'yum', '>= 3.0'
+depends 'yum-epel', '>= 0.2'
