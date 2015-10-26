3ac2ea6ebc9db055eeded9a243b5402255778c7b
Fraser Scott <fraser.scott@gmail.com>
2015-10-26 15:20:15 +0000

    retry tuning

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,8 @@ default['sensu_spec']['plugin_dir'] = '/etc/sensu/plugins'
 default['sensu_spec']['nagios']['plugins_path'] = '/usr/lib/nagios/plugins/'
 
 default['sensu_spec']['default_command_path'] = default['sensu_spec']['nagios']['plugins_path']
-default['sensu_spec']['retry_count'] = 0
-default['sensu_spec']['retry_sleep'] = 1.0
+default['sensu_spec']['retry_count'] = 3
+default['sensu_spec']['retry_sleep'] = 5.0
 
 default.sensu_spec.client_defaults.subscriptions = ['default']
 default.sensu_spec.check_defaults.handlers = ['default']
